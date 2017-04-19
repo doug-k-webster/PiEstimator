4e0d11ed2cd5564ee0714600798f7f3ef66c817d
bugspray <doug.k.webster@gmail.com>
2017-04-19 16:56:33 -0700

    Added code

--- PiEstimator/Program.cs
+++ PiEstimator/Program.cs
@@ -1,15 +1,79 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace PiEstimator
+﻿namespace PiEstimator
 {
+    using System;
+    using System.Numerics;
+
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            // GregoryLeibnizSeries();
+            // NilakanthaSeries();
+            NilakanthaSeriesBigInt();
+            Console.ReadKey();
+        }
+
+        private static void NilakanthaSeries()
+        {
+            // Nilakantha series
+            double pi = 3d;
+
+            double sign = 1;
+
+            for (double i = 2; i < int.MaxValue; i += 2)
+            {
+                double delta = sign * 4d / (i * (i + 1) * (i + 2));
+                pi = pi + delta;
+
+                if ((int) i % 10000000 == 0)
+                {
+                    Console.WriteLine("i: {0} pi: {1}", i, pi);
+                }
+
+                sign = sign * -1;
+            }
+        }
+
+        private static void NilakanthaSeriesBigInt()
+        {
+            // Nilakantha series
+            BigInteger sign = 1;
+            BigInteger multiplier = BigInteger.Pow(10, 100);
+
+            BigInteger pi = 3 * multiplier;
+            for (BigInteger i = 2; i < int.MaxValue; i += 2)
+            {
+                BigInteger delta = multiplier * sign * 4 / (i * (i + 1) * (i + 2));
+                pi = pi + delta;
+
+                if ((int) i % 10000000 == 0)
+                {
+                    Console.WriteLine("i: {0} pi: {1}", i, pi);
+                }
+
+                sign = sign * -1;
+            }
+        }
+
+        private static void GregoryLeibnizSeries()
+        {
+            // Gregory–Leibniz series
+            double pi = 0d;
+
+            double sign = 1;
+
+            for (double i = 1; i < int.MaxValue; i += 2)
+            {
+                double delta = sign * 4d / i;
+                pi = pi + delta;
+
+                if (((int) i - 1) % 10000000 == 0)
+                {
+                    Console.WriteLine("i: {0} pi: {1}", i, pi);
+                }
+
+                sign = sign * -1;
+            }
         }
     }
 }
